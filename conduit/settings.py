036b714096ae2ade390350e6444f37f6981e6349
A D Vishnu Prasad <advishnuprasad@gmail.com>
2019-09-11 17:44:41 +0530

    Initial commit

--- conduit/settings.py
+++ conduit/settings.py
@@ -126,3 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'authentication.User'
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'conduit.apps.core.exceptions.core_exception_handler',
+    'NON_FIELD_ERRORS_KEY': 'error',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'conduit.apps.authentication.backends.JWTAuthentication',
+    ),
+}
